fb4c482861426d34592a6e0c3f0f75da988fc739
eason <easonsyx@gmail.com>
2020-03-06 23:54:18 -0500

    Issue #2: Object-oriented Design Improvements

--- app/src/main/java/com/example/enterprisetaskscheduler/Task.java
+++ app/src/main/java/com/example/enterprisetaskscheduler/Task.java
@@ -1,7 +1,7 @@
 package com.example.enterprisetaskscheduler;
 
 
-public class Task {
+public class Task extends ListItem{
     private String taskName;
     private String empName;
     private String description;
@@ -9,14 +9,12 @@ public class Task {
     private String endDate;
     private String status;
     private int empId;
-    private int id = -1;
 
 
     public Task(String taskName, String startDate, String endDate, int empId) {
         super();
         this.taskName = taskName;
         this.empId = empId;
-        this.description = description;
         this.startDate = startDate;
         this.endDate = endDate;
         this.status = "On Going";
@@ -50,32 +48,17 @@ public class Task {
         return empName;
     }
 
-    public int getId() {
-        return id;
-    }
-
     public void setEmpName(String empName) {
         this.empName = empName;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public void setDescription(String description) {
         this.description = description;
     }
 
-    private String format(String s) {
-        if (!s.equals("")) {
-            return s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
-        }
-        return s;
-    }
-
     public boolean contains(String s) {
         s = s.toLowerCase();
-        return Integer.toString(id).contains(s) ||
+        return Integer.toString(super.getId()).contains(s) ||
                 taskName.toLowerCase().contains(s) ||
                 empName.toLowerCase().contains(s) ||
                 endDate.toLowerCase().contains(s);
